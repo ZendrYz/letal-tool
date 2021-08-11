72a706a695deb75c042611061be0e3800bd2a5f5
ZendrYz Aguilar <xghoulzfn@gmail.com>
2021-08-11 17:27:19 +0200

    hola

--- letal-tool.py
+++ letal-tool.py
@@ -31,15 +31,47 @@ def main():
     print(Fore.WHITE + "Tool de Letal Net")
     print(Fore.WHITE + "Versión 1.1 | Coded by ZendrYz")
     print()
-    print(Fore.GREEN + "[1] GEOLOCALIZACIÓN POR IP")
-    print(Fore.GREEN + "[2] GEN + CHECKER DE NITRO")
+    print(Fore.GREEN + "[1] CANAL DE KRYSSI$")
+    print(Fore.GREEN + "[2] TIK TOK DE LETAL NET")
     print(Fore.GREEN + "[3] CONSEGUIR IP DE UN DOMINIO")
-    print(Fore.GREEN + "[4] CANAL DE KRYSSI$")
-    print(Fore.GREEN + "[5] TIK TOK DE LETAL NET")
+    print(Fore.GREEN + "[4] GEOLOCALIZACIÓN POR IP")
+    print(Fore.GREEN + "[5] GEN + CHECKER DE NITRO")
 
     menu = input(Fore.WHITE + "Inserte una opción >> ")
 
     if menu == "1":
+
+        webbrowser.open(
+            "https://www.youtube.com/channel/UCbpIrs3r8mLoMIi-BGvZFbA", new=2, autoraise=True)
+        os.system("cls")
+        main()
+
+    if menu == "2":
+
+        webbrowser.open(
+            "https://www.tiktok.com/@letalnet?lang=es&is_copy_url=1&is_from_webapp=v1", new=2, autoraise=True)
+        os.system("cls")
+        main()
+
+    if menu == "3":
+
+        os.system("cls")
+        ip2 = input(
+            Fore.RED + "[Letal Net TOOL] Inserte aqui la URL de la web >> " + Fore.GREEN)
+
+        print()
+        ip = socket.gethostbyname(str(ip2))
+        print(Fore.YELLOW + "La direccion IP es >> " + Fore.BLUE + str(ip))
+
+        print()
+        menu2331 = input(
+            Fore.WHITE + "Presione la tecla intro para volver al menu...")
+
+        os.system("cls")
+
+        main()
+
+    if menu == "4":
 
         os.system("cls")
 
@@ -71,23 +103,22 @@ def main():
         os.system("cls")
         main()
 
-    if menu == "2":
-
-        os.system("cls")
-        num = input(
+    if menu == "5":
+         os.system("cls")
+         num = input(
             Fore.RED + '[Letal Net TOOL] Teclea cuantos codigos desea generar y checkear. Para verlos mejor se te creará un archivo llamado Nitro Codes.txt donde los podrás ver con más claridad >> ')
 
-    f = open("Nitro Codes.txt", "w", encoding='utf-8')
+         f = open("Nitro Codes.txt", "w", encoding='utf-8')
 
-    for n in range(int(num)):
-        y = ''.join(random.choice(string.ascii_uppercase +
+         for n in range(int(num)):
+          y = ''.join(random.choice(string.ascii_uppercase +
                     string.digits + string.ascii_lowercase) for _ in range(24))
-        f.write('https://discord.gift/')
-        f.write(y)
-        f.write("\n")
+         f.write('https://discord.gift/')
+         f.write(y)
+         f.write("\n")
 
-    with open("Nitro Codes.txt") as f, open('Valid Codes.txt', 'w', encoding="UTF-8") as valid:
-        for line in f:
+         with open("Nitro Codes.txt") as f, open('Valid Codes.txt', 'w', encoding="UTF-8") as valid:
+          for line in f:
             nitro = line.strip("\n")
 
             url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + \
@@ -104,41 +135,10 @@ def main():
                 print(" Invalid | {} ".format(line.strip("\n")))
                 break
 
-    os.system("cls")
-    main()
+                os.system("cls")
 
-    if menu == "3":
-
-        os.system("cls")
-        ip2 = input(
-            Fore.RED + "[Letal Net TOOL] Inserte aqui la URL de la web >> " + Fore.GREEN)
-
-        print()
-        ip = socket.gethostbyname(str(ip2))
-        print(Fore.YELLOW + "La direccion IP es >> " + Fore.BLUE + str(ip))
-
-        print()
-        menu2331 = input(
-            Fore.WHITE + "Presione la tecla intro para volver al menu...")
-
-        os.system("cls")
-
-        main()
-
-    if menu == "4":
-
-        webbrowser.open(
-            "https://www.youtube.com/channel/UCbpIrs3r8mLoMIi-BGvZFbA", new=2, autoraise=True)
-        os.system("cls")
-        main()
-
-    if menu == "5":
-
-        webbrowser.open(
-            "https://www.tiktok.com/@letalnet?lang=es&is_copy_url=1&is_from_webapp=v1", new=2, autoraise=True)
-        os.system("cls")
-        main()
-
+                main()
+       
     else:
         os.system("cls")
         print(Fore.RED + "¡Has introducido un numero incorrecto!")
